20b82eec8904fdd708f0f346897b2da8ec4ba458
Mohmmadmehdi21 <mohmmadmehdi44@gmail.com>
2026-09-18 19:45:18 +0530

    loginpage connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dashboard.dart';
+import 'loginscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Workshop',
-      home: const Dashboard(),
+      home: LoginScreen(),
     );
   }
 }
